4dd5fedff0b1944f3ffd2f71b84e7bd3e723c500
lannyrivero <lannyrivero288@gmail.com>
2026-02-06 12:58:24 +0100

    feat: implement ChatService for handling user chat interactions

---
+++ lab1-chat-client/src/main/java/dev/lanny/lab_chat_client/chat/ChatService.java
@@ -0,0 +1,22 @@
+package dev.lanny.lab_chat_client.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+
+    private final ChatClient chatClient;
+
+    public ChatService(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    public String chat(String userMessage) {
+        return chatClient
+                .prompt()
+                .user(userMessage)
+                .call()
+                .content();
+    }
+}
