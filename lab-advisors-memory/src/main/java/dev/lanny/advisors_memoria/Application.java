2b46906f8617d008bd1fb4f287735aebb23a6232
lannyrivero <lannyrivero288@gmail.com>
2026-02-10 22:26:24 +0100

    feat: initialize lab-advisors-memory with Spring Boot setup and Maven wrapper

---
+++ lab-advisors-memory/src/main/java/dev/lanny/advisors_memoria/Application.java
@@ -0,0 +1,13 @@
+package dev.lanny.advisors_memoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
