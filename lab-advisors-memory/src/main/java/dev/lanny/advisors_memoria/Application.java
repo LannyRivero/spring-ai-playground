0b27c27af6c1cfcba56f6c449867be542439bde9
lannyrivero <lannyrivero288@gmail.com>
2026-02-11 12:09:50 +0100

    feat: enable configuration properties for MemoryProperties in Application class

--- lab-advisors-memory/src/main/java/dev/lanny/advisors_memoria/Application.java
+++ lab-advisors-memory/src/main/java/dev/lanny/advisors_memoria/Application.java
@@ -2,8 +2,12 @@ package dev.lanny.advisors_memoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import dev.lanny.advisors_memoria.config.MemoryProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MemoryProperties.class)
 public class Application {
 
 	public static void main(String[] args) {
